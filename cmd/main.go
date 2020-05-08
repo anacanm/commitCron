5c61185affc811a7b42e4abc29e9aed45c543a84
anacanm <anacan@mangelsdorf.io>
2020-05-08 11:45:10 -0700

    Add main GetRepoContents functionality, need to refactor so that it doesn't share memory
    Currently GetRepoContents holds a reference to a slice that the main goroutine also holds access to. I need to refactor GetRepoContents so that it will only send this along a channel, and be wary of allocations when doing so
    
    This commit does not hold a fully working codebase, it is a checkpoint-type save

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -22,24 +23,39 @@ func main() {
 	}
 
 	// create an http Client with a 7 second timeout to be used by all goroutines:
-	// From https://golang.org/src/net/http/client.go: 
+	// From https://golang.org/src/net/http/client.go:
 	// "Clients should be reused instead of created as needed. Clients are safe for concurrent use by multiple goroutines."
 	client := &http.Client{
 		Timeout: time.Second * 7,
 	}
 
 	// nContributions is an unbuffered channel that will receive the numberOfContributions
-	contributionChannel := make(chan contributions.ContributionItem) 
+	contributionChannel := make(chan contributions.ContributionItem)
 
+	// TODO: make numberOfChangesToMake either user-inputted or a random number within a range to provide the illusion of human commits
+	numberOfChangesToMake := 5
 
 	go contributions.GetNumberOfContributionsToday(client, contributionChannel)
 
+	errorChan := make(chan error)
+	finishGetRepo := make(chan struct{})
 
+	repoContentsURL := fmt.Sprintf("https://api.github.com/repos/%v/%v/contents", os.Getenv("GITHUB_USERNAME"), os.Getenv("REPO_NAME"))
+
+	// TODO: change GetRepoContents to send the result slice over a channel, rather than sharing memory.
+	// "Don't communicate by sharing memory, share memory by communicating": https://www.youtube.com/watch?v=PAAkCSZUG1c&t=2m48s
+
+	// * NOTE: Initialize result with a capacity of numberOfChangesToMake so that no additional allocation will be needed
+
+	// TODO: if a message from main is sent to finishGetRepo, finishGetRepo needs to be drained and closed
+	go func() {
+		defer close(errorChan)
+		GetRepoContents(repoContentsURL, make([]RepoContent, 0, numberOfChangesToMake), numberOfChangesToMake, client, finishGetRepo, errorChan)
+	}()
 	contributionResult := <-contributionChannel
 	if contributionResult.Err != nil {
 		log.Fatalf("Error getting contributions: %v", contributionResult.Err)
 	}
-
 
 	// if the user has committed less than 4 times, make somewhere between 4 and 8 (random, inclusive) commits to give the illusion of normal commits
 	// 4 is an arbitrary number that I chose so that if I'd already made plenty of commits on a day, that I wouldn't overdo my commits
--- cmd/repo.go
+++ cmd/repo.go
@@ -35,43 +35,64 @@ func fileCanBeModified(fileName string) bool {
 }
 
 // GetRepoContents sends (on the out channel) the first n RepoContents in a repository that are able to be modified (ie. not dirs or important files)
+// TODO: update documentation (mainly the func doc)
 // if the RepoContents are no longer needed (signaled by the finish channel), then function exits
 // (this occurs when the first concurrent request to contributions.GetNumberOfContributionsToday sends a number higher than the upper bound for daily )
-func GetRepoContents(url string, result []RepoContent, nRequiredContents int, beenThere map[string]bool, client *http.Client, finish <-chan struct{}, out chan<- error) {
+func GetRepoContents(url string, result []RepoContent, nRequiredContents int, client *http.Client, finish chan struct{}, errorChan chan<- error) {
 	select {
 	// the use of select here is to have a nonblocking receive check for finish, if no finish message has  been set, proceed with the operation
 	case <-finish:
-		// despite the fact that this is a recursive function, I am able to use close(out) here knowing that it will not get called more than once
-		// there will ever only be one message sent to this finish channel, so this statement will execute only one time
-		close(out)
-		return
-	default:
-		// update the beenThere array for the current directory
-		beenThere[url] = true
+		// this case needs to initiate the recursive exit of the function
+		// it does so by sending an additional message to the finish channel before it returns, which will be consumed by the function below it on the call stack, and a new one will be sent.
+		// this means that the last message sent (before the root level function is returned from), will not be consumed
+		// this helps reduce the resources consumed, and will safely exit the function so that the goroutine calling it may exit
 
+		// ! NOTE: it may be better to create a select { case <-finish } in the loop that recursively calls this function so that no new HTTP requests are made and to keep the growth of this goroutine to a minimum
+		finish <- struct{}{}
+		// TODO: below
+		// * need to close the out channel outisde of GetRepoContents
+		// * need to drain and close the finish channel outside of GetRepoContents, (there will be one message remaining after the last return statement)
+		return
+
+	default:
+		if len(result) == nRequiredContents {
+			// proper exit case
+			return
+		}
+
+
+		// create new HTTP request
 		req, err := http.NewRequest("GET", url, nil)
 		if err != nil {
-			out <- fmt.Errorf("Error creating http GET request for %v: %v", url, err)
+			errorChan <- fmt.Errorf("Error creating http GET request for %v: %v", url, err)
 		}
 
+		// add Authorization header with user's github api token
+		// for info on creating an api token: https://github.com/settings/tokens
+		// for this project, the api token needs access to the full repo scope
 		req.Header.Add("Authorization", fmt.Sprintf("token %v", os.Getenv("GITHUB_API_TOKEN")))
 
+		// send request
 		resp, err := client.Do(req)
 		if err != nil {
-			out <- fmt.Errorf("Error sending http GET request for %v: %v", url, err)
+			errorChan <- fmt.Errorf("Error sending http GET request for %v: %v", url, err)
 		}
 
-		// I cannot use "defer resp.Body.Close()" because of this function's recursive-ness and defer's last called, first executed nature: https://tour.golang.org/flowcontrol/13
-		// if defer were used, the last called response body would be closed, then the second last called response body, and so on until finally the first called response was closed last.
-		// It is better to explicitly repeat myself more than to use defer and allow the resource leek from keeping the connection open.
+		// * NOTE: I cannot use "defer resp.Body.Close()" because of this function's recursive-ness and defer's last called, first executed nature: https://tour.golang.org/flowcontrol/13
+		// * if defer were used, the last called response body would be closed, then the second last called response body, and so on until finally the first called response was closed last.
+		// * It is better to explicitly repeat myself more than to use defer and allow the resource leek from keeping the connection open.
 
-		//shallowResult is a temporary location to decode the response from the github api into. It is from this shallowResult that we can filter through the data
+		//shallowResult is a temporary location to decode the response from the github api request into. It is from this shallowResult that we can filter through the data
 		var shallowResult []RepoContent
 		err = json.NewDecoder(resp.Body).Decode(&shallowResult)
 		if err != nil {
-			out <- fmt.Errorf("Error decoding json response from %v into []RepoContent: %v", url, err)
+			errorChan <- fmt.Errorf("Error decoding json response from %v into []RepoContent: %v", url, err)
 			resp.Body.Close()
 		}
+
+		// although iterating over shallowResult two separate times has a complexity of 0(2n), I believe that due to the nature of directories being small in breadth
+		// n should never get to be large enough such that the complexity would result in a negative impact on performance
+		// I weigh the clarity of the two separate iterations to be more important than the possible minimal performance benefit from a more efficient traversal
 
 		for _, value := range shallowResult {
 			// if the number of files that are desired to be updated have been reached, initiate exit
@@ -79,23 +100,30 @@ func GetRepoContents(url string, result []RepoContent, nRequiredContents int, be
 				resp.Body.Close()
 				return
 			}
-			// otherwise, check if the file is a file and if it is allowed to be modified, and append it to the list of files to be modified
+			// otherwise, check if the value is a file and if it is allowed to be modified, and append it to the list of files to be modified
 			if value.Type == "file" && fileCanBeModified(value.Name) {
 				result = append(result, value)
+				// result only ever grows by 1 at a time, so I can safely check len(result) == nRequiredContents without having to worry about passing nRequiredContents
 			}
 		}
 
 		// if this is reached, then the current directory of the tree has no more files that can be updated, so we must proceed a level deeper
+		// we do so by recursing to a new subdirectory in the repository, which requires a new HTTP request to the api specifying that we want the new subdirectory
 		for _, value := range shallowResult {
-			if value.Type == "dir" && !beenThere[value.Links.Self] {
+			// if the number of files that are desired to be updated have been reached, initiate exit
+			if len(result) == nRequiredContents {
 				resp.Body.Close()
-				GetRepoContents(value.Links.Self, result, nRequiredContents, beenThere, client, finish, out)
+				return
+			}
+			if value.Type == "dir" {
+				GetRepoContents(value.Links.Self, result, nRequiredContents, client, finish, errorChan)
 			}
 		}
 
-		// if this is reached then the current directory of the tree has no more files that can be updated, nor subdirectories, so now we must return up a level
-		// TODO: implement tree structure that will be updated as it is traversed
+
+		// if this is reached then the current directory of the tree has no more files that can be updated, nor subdirectories, so now we return up a level
 		resp.Body.Close()
+		return
 	}
 
 }
