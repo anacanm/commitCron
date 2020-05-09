345c449b2916a1850307383335057e7e6bdb67f2
anacanm <anacan@mangelsdorf.io>
2020-05-08 23:06:50 -0700

    Add complete, optimized getRepoContents function

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -29,41 +30,88 @@ func main() {
 		Timeout: time.Second * 7,
 	}
 
-	// nContributions is an unbuffered channel that will receive the numberOfContributions
+	// contributionChannel is an unbuffered channel that will receive the numberOfContributions
 	contributionChannel := make(chan contributions.ContributionItem)
 
-	// TODO: make numberOfChangesToMake either user-inputted or a random number within a range to provide the illusion of human commits
-	numberOfChangesToMake := 5
+	// TODO: make numberOfContributionsToMake either user-inputted or a random number within a range to provide the illusion of human commits
+	numberOfContributionsToMake := 5
 
 	go contributions.GetNumberOfContributionsToday(client, contributionChannel)
 
-	errorChan := make(chan error)
-	finishGetRepo := make(chan struct{})
-
-	repoContentsURL := fmt.Sprintf("https://api.github.com/repos/%v/%v/contents", os.Getenv("GITHUB_USERNAME"), os.Getenv("REPO_NAME"))
-
-	// TODO: change GetRepoContents to send the result slice over a channel, rather than sharing memory.
 	// "Don't communicate by sharing memory, share memory by communicating": https://www.youtube.com/watch?v=PAAkCSZUG1c&t=2m48s
 
-	// * NOTE: Initialize result with a capacity of numberOfChangesToMake so that no additional allocation will be needed
+	// * NOTE: Initialize result with a capacity of numberOfContributionsToMake so that no additional allocation will be needed
 
 	// TODO: if a message from main is sent to finishGetRepo, finishGetRepo needs to be drained and closed
+	repoContentsURL := fmt.Sprintf("https://api.github.com/repos/%v/%v/contents", os.Getenv("GITHUB_USERNAME"), os.Getenv("REPO_NAME"))
+
+	// ! all of the channels used by GetRepoContents should be buffered so that the function can send the necessary message (whether it be an error or result) and immediately begin termination
+	getRepoOutput := make(chan []RepoContent, 2)
+	terminateGetRepo := make(chan struct{}, 1)
+	getRepoContentsErrorChan := make(chan error, 1)
+
 	go func() {
-		defer close(errorChan)
-		GetRepoContents(repoContentsURL, make([]RepoContent, 0, numberOfChangesToMake), numberOfChangesToMake, client, finishGetRepo, errorChan)
+		// GetRepoContents is wrapped in this anonymous function because it is recursive and therefore calling defer close(channelName) would not work well.
+		// Therefore, it is best to simply wrap it in a small anonymous function that gives the flexibility desired
+
+		// NOTE: cannot call defer close(getRepoOutput) or defer close(getRepoContentsErrorChan) until after the below select statement because a closed channel never blocks
+		// this means that in the below select case, if the function were to have succeeded sending the data AND terminating before the select statement was reached, the error channel would be closed
+		// , and therefore readable from (reading it will return a nil error when one was never sent), so it would be selected when no error was sent.
+
+		defer close(terminateGetRepo)
+
+		GetRepoContents(repoContentsURL, make([]RepoContent, 0, numberOfContributionsToMake), numberOfContributionsToMake, client, getRepoOutput, terminateGetRepo, getRepoContentsErrorChan)
 	}()
+
 	contributionResult := <-contributionChannel
 	if contributionResult.Err != nil {
 		log.Fatalf("Error getting contributions: %v", contributionResult.Err)
 	}
 
-	// if the user has committed less than 4 times, make somewhere between 4 and 8 (random, inclusive) commits to give the illusion of normal commits
 	// 4 is an arbitrary number that I chose so that if I'd already made plenty of commits on a day, that I wouldn't overdo my commits
-	if contributionResult.NumberContributions < 4 {
+	if contributionResult.NumberContributions < 100 {
+		// if we want to make contributions, we need to gracefully handle possible errors, and then procede
+		select {
+		case err := <-getRepoContentsErrorChan:
+			// close the channels,
+			close(getRepoContentsErrorChan)
+			close(getRepoOutput)
+			log.Fatalf("Error getting repo contents from %v: %v", repoContentsURL, err)
+
+		case contents := <-getRepoOutput:
+			json, err := json.MarshalIndent(contents, "", "	")
+			if err != nil {
+				log.Fatalf("Error marshalling json data: %v", err)
+			}
+			close(getRepoContentsErrorChan)
+			close(getRepoOutput)
+			fmt.Println(string(json))
+			/*
+				if len(contents) == numberOfContributionsToMake {
+					for _, v := range contents {
+						if err := UpdateFile(os.Getenv("REPO_NAME"), v.Path, client); err != nil {
+							log.Fatalf("Error updating %v in %v: %v", v.Path, os.Getenv("REPO_NAME"), err)
+						}
+					}
+				}
+			*/
+		}
 		// repoName is the repository that you want to access
 		// path to file is the relative (relative to the repo) path that
-		if err := UpdateFile(os.Getenv("REPO_NAME"), os.Getenv("PATH_TO_FILE"), client); err != nil {
-			log.Fatalf("Error updating %v in %v: %v", os.Getenv("PATH_TO_FILE"), os.Getenv("REPO_NAME"), err)
+	} else {
+		// if we do not in fact want to make any contributions, since we have achieved our daily quota, then we should instruct the function to terminate
+		// there are three distinct states that GetRepoContents can be in:
+		// 	1. it has found an error, communicated it over the channel, and begun termination on its own. In this case, we should do nothing more then drain the error
+		//	2. it has already completed getting the desired content from the repo, communicated it over the channel, and begun termination on its own. Again, do nothing other than drain
+		//	3. neither an error or completion has occured: we should instruct the function to terminate gracefully as it is no longer needed
+		select {
+		case <-getRepoContentsErrorChan:
+			// do nothing, it is cleaning itself up
+		case <-getRepoOutput:
+			// do nothing, it is cleaning itself up
+		default:
+			// instruct the function to terminate
+			terminateGetRepo <- struct{}{}
 		}
 	}
 }
--- cmd/repo.go
+++ cmd/repo.go
@@ -36,35 +36,41 @@ func fileCanBeModified(fileName string) bool {
 
 // GetRepoContents sends (on the out channel) the first n RepoContents in a repository that are able to be modified (ie. not dirs or important files)
 // TODO: update documentation (mainly the func doc)
-// if the RepoContents are no longer needed (signaled by the finish channel), then function exits
+// if the RepoContents are no longer needed (signaled by the terminate channel), then function exits
 // (this occurs when the first concurrent request to contributions.GetNumberOfContributionsToday sends a number higher than the upper bound for daily )
-func GetRepoContents(url string, result []RepoContent, nRequiredContents int, client *http.Client, finish chan struct{}, errorChan chan<- error) {
+func GetRepoContents(url string, result []RepoContent, nRequiredContents int, client *http.Client, output chan []RepoContent, terminate chan struct{}, errorChan chan<- error) {
 	select {
-	// the use of select here is to have a nonblocking receive check for finish, if no finish message has  been set, proceed with the operation
-	case <-finish:
+	// the use of select here is to have a nonblocking receive check for terminate, if no terminate message has  been set, proceed with the operation
+	case <-terminate:
 		// this case needs to initiate the recursive exit of the function
-		// it does so by sending an additional message to the finish channel before it returns, which will be consumed by the function below it on the call stack, and a new one will be sent.
+		// it does so by sending an additional message to the terminate channel before it returns, which will be consumed by the function below it on the call stack, and a new one will be sent.
 		// this means that the last message sent (before the root level function is returned from), will not be consumed
 		// this helps reduce the resources consumed, and will safely exit the function so that the goroutine calling it may exit
 
-		// ! NOTE: it may be better to create a select { case <-finish } in the loop that recursively calls this function so that no new HTTP requests are made and to keep the growth of this goroutine to a minimum
-		finish <- struct{}{}
+		// ! NOTE: it may be better to create a select { case <-terminate } in the loop that recursively calls this function so that no new HTTP requests are made and to keep the growth of this goroutine to a minimum
+		terminate <- struct{}{}
 		// TODO: below
 		// * need to close the out channel outisde of GetRepoContents
-		// * need to drain and close the finish channel outside of GetRepoContents, (there will be one message remaining after the last return statement)
+		// * need to drain and close the terminate channel outside of GetRepoContents, (there will be one message remaining after the last return statement)
 		return
 
 	default:
 		if len(result) == nRequiredContents {
-			// proper exit case
+			// * proper exit case: send the result to the output channel, initiate termination
+			// the output channel should not block on send (have a buffer where length < capacity), so that this function will be able to send the result and immediately begin termination quietly on its own.
+
+			// If the output channel is blocking (which is not desired), then the goroutine that this is spawned in will have to wait until someone receives on the output channel to begin termination
+			output <- result
+			terminate <- struct{}{}
 			return
 		}
-
 
 		// create new HTTP request
 		req, err := http.NewRequest("GET", url, nil)
 		if err != nil {
 			errorChan <- fmt.Errorf("Error creating http GET request for %v: %v", url, err)
+			terminate <- struct{}{}
+			return
 		}
 
 		// add Authorization header with user's github api token
@@ -76,6 +82,8 @@ func GetRepoContents(url string, result []RepoContent, nRequiredContents int, cl
 		resp, err := client.Do(req)
 		if err != nil {
 			errorChan <- fmt.Errorf("Error sending http GET request for %v: %v", url, err)
+			terminate <- struct{}{}
+			return
 		}
 
 		// * NOTE: I cannot use "defer resp.Body.Close()" because of this function's recursive-ness and defer's last called, first executed nature: https://tour.golang.org/flowcontrol/13
@@ -87,7 +95,9 @@ func GetRepoContents(url string, result []RepoContent, nRequiredContents int, cl
 		err = json.NewDecoder(resp.Body).Decode(&shallowResult)
 		if err != nil {
 			errorChan <- fmt.Errorf("Error decoding json response from %v into []RepoContent: %v", url, err)
+			terminate <- struct{}{}
 			resp.Body.Close()
+			return
 		}
 
 		// although iterating over shallowResult two separate times has a complexity of 0(2n), I believe that due to the nature of directories being small in breadth
@@ -97,6 +107,8 @@ func GetRepoContents(url string, result []RepoContent, nRequiredContents int, cl
 		for _, value := range shallowResult {
 			// if the number of files that are desired to be updated have been reached, initiate exit
 			if len(result) == nRequiredContents {
+				output <- result
+				terminate <- struct{}{}
 				resp.Body.Close()
 				return
 			}
@@ -110,18 +122,42 @@ func GetRepoContents(url string, result []RepoContent, nRequiredContents int, cl
 		// if this is reached, then the current directory of the tree has no more files that can be updated, so we must proceed a level deeper
 		// we do so by recursing to a new subdirectory in the repository, which requires a new HTTP request to the api specifying that we want the new subdirectory
 		for _, value := range shallowResult {
-			// if the number of files that are desired to be updated have been reached, initiate exit
-			if len(result) == nRequiredContents {
-				resp.Body.Close()
+			select {
+			// to prevent unecessary recursive calls being made, check to see if a terminate message has been received
+			// this select case offers a slight improvement: although the general select case that wraps this entire function ensures that no extra operations are performed in that function call if a terminate message is received,
+			// it does not prevent unnecessary calls that will be immediately rejected, which is what this select case does
+			// this select case becomes helpful when a terminate message has been sent between the timeframe of the function call beginning and when this loop is finished: if that were the case,
+			// then without this select case, each subdirectory iterated over (assuming that len(result) < nRequiredContents) would result in a new recursive call to GetRepoContents,
+			// which would immediately be exited early from due to the general select case statement wrapping the function.
+			// Therefore, this select statement prevents the pointless growing of the call stack, and allows the function to initiate termination immediately
+			case <-terminate:
+				// if the terminate message has been received, initiate termination by sending a terminate message to the call below this on the call stack and exit this recusive call
+				terminate <- struct{}{}
 				return
-			}
-			if value.Type == "dir" {
-				GetRepoContents(value.Links.Self, result, nRequiredContents, client, finish, errorChan)
+
+			default:
+				// if the number of files that are desired to be updated have been reached, initiate exit
+				if len(result) == nRequiredContents {
+					output <- result
+					terminate <- struct{}{}
+					resp.Body.Close()
+					return
+				}
+				if value.Type == "dir" {
+					GetRepoContents(value.Links.Self, result, nRequiredContents, client, output, terminate, errorChan)
+				}
 			}
 		}
 
+		// if this is reached then the current directory of the tree has no more files that can be updated, nor subdirectories to go into, so now we return up a level in the directory structure (down on the call stack)
 
-		// if this is reached then the current directory of the tree has no more files that can be updated, nor subdirectories, so now we return up a level
+		// if the function is about to return from the root directory, then send whatever content has been accumulated
+		// this is to ensure that even if the number of modifiable files is less than nRequiredContents, that the modifiable content (if any) is sent
+		// however, if content has already been sent, then this will be a duplicate send, but this is okay since the channel will only be read from once
+		// the output channel should have a buffer of 2 so that in the case of a second send, the function does not block
+		if url == fmt.Sprintf("https://api.github.com/repos/%v/%v/contents", os.Getenv("GITHUB_USERNAME"), os.Getenv("REPO_NAME")) {
+			output <- result
+		}
 		resp.Body.Close()
 		return
 	}
